9125f3e6b14067bf5c30fa9a71f4e724b6d3e0c6
Oliver Katz <code.sn1@gmail.com>
2013-04-30 21:29:35 -0500

    cryptography bits are written now

--- crypt.h
+++ crypt.h
@@ -1,3 +1,21 @@
 #ifndef __SOUNDING_CRYPT_H
 #define __SOUNDING_CRYPT_H
 
+#include <stdio.h>
+
+extern char *sounding_crypt_dir;
+
+unsigned int sounding_timestamp();
+
+char *sounding_generate_path(char *prefix, char *ext);
+int sounding_file_write(char *path, void *data, size_t size);
+int sounding_file_read(char *path, void **data, size_t *size);
+
+int sounding_sign(void **data, size_t *size);
+int sounding_unsign(void **data, size_t *size);
+int sounding_encrypt(char *reciepient, void **data, size_t *size);
+int sounding_decrypt(void **data, size_t *size);
+int sounding_export_key(char *user, void **data, size_t *size);
+int sounding_import_key(void **data, size_t *size);
+
+#endif
